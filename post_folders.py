5f7a8d1f3d9bbe023d3f1d168e2918884cedefa8
Shintaro Yamaoka <s.yamaoka927@gmail.com>
2023-05-11 16:07:59 +0900

    second

--- post_folders.py
+++ post_folders.py
@@ -12,12 +12,29 @@ headers = {
 }
 
 body = {
-  "name": f"{app.name} (Order ID: {str(app.order_id)})",
+  "id": app.order_id,
+  "name": f"{app.name} (Order ID: {str(app.order_id)}) Test15",
   "parent": {
     "id": "202361405223"
+  },
+  "shared_link": {
+    "vanity_name": str(app.order_id),
+    "access": "open",
+    "permissions": {
+      "can_download": True
+    }
   }
 }
 
 res = requests.post(URL, headers=headers, json=body)
-status = res.status_code
-print(status)
+
+result = res.json()
+id = result["id"]
+
+URL = f"https://api.box.com/2.0/folders/{id}?fields=shared_link"
+
+res = requests.put(URL, headers=headers, json=body)
+result = res.json(
+link = result["shared_link"]
+# link_url = link["vanity_url"]
+print(link)
